c590b5fb8343c1890b430a37fb13f560fc791d65
Antonio Escorcia <packageinstaller2@gmail.com>
2022-01-09 10:40:17 -0600

    Changes in Game class
    constant for frame handling were added, constants for screen sizing werer added, now the game has a gameobject collection which uses to handle input, update and draw per object, game closes when escape key is pressed, the game will run at most 60 FPS, game calculates deltaTime, game has new methods for adding and removing game objects to the game

--- game.py
+++ game.py
@@ -2,20 +2,23 @@ import pygame
 
 class Game():
 
-    SCREEN_WIDTH = 1024
-    SCREEN_HEIGHT = 768
+    FRAMES_PER_SECOND = 60
+    MILISECONDS_IN_SECOND = 1000
+    MILISECONDS_BETWEEN_FRAMES = MILISECONDS_IN_SECOND / FRAMES_PER_SECOND
 
-    @property
-    def initialized(self):
-        return pygame.get_init()
-
+    SCREEN_WIDTH = 500
+    SCREEN_HEIGHT = 500
 
     def __init__(self):
         pygame.init()
 
         self.screen = pygame.display.set_mode([Game.SCREEN_WIDTH, Game.SCREEN_HEIGHT])
 
-        self.isRunning = True
+        self.gameObjects = []
+        self.tickCount = 0
+        self.clock = pygame.time.Clock()
+
+        self.isRunning = pygame.get_init()
 
     
     def handle_input(self):
@@ -25,15 +28,34 @@ class Game():
             if event.type == pygame.QUIT:
                 self.isRunning = False
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.isRunning = False
+
+            
+            for gameObject in self.gameObjects:
+                gameObject.handle_input(event)
+
 
     def update(self):
-        self.screen.fill((0, 0, 0))
 
-        pygame.display.flip()
+        self.clock.tick(Game.FRAMES_PER_SECOND)
+
+        deltaTime = (pygame.time.get_ticks() - self.tickCount) / Game.MILISECONDS_IN_SECOND
+        self.tickCount = pygame.time.get_ticks()
+        
+        for gameObject in self.gameObjects:
+            gameObject.update(deltaTime)
 
 
     def generate_output(self):
-        pass
+
+        self.screen.fill((0, 0, 0))
+        
+        for gameObject in self.gameObjects:
+            gameObject.draw(self.screen)
+
+        pygame.display.flip()
 
 
     def run_loop(self):
@@ -41,6 +63,14 @@ class Game():
             self.handle_input()
             self.update()
             self.generate_output()
+
+
+    def add_game_object(self, game_object):
+        self.gameObjects.append(game_object)
+    
+
+    def remove_game_object(self, game_object):
+        self.gameObjects.remove(game_object)
 
 
     def shutdown(self):
