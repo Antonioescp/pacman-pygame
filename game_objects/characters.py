7c032fb6ecebb4e11f93551485543379666ceb30
Antonio Escorcia <packageinstaller2@gmail.com>
2022-01-10 17:54:09 -0600

    Ghost random movement on wall collision
    When a Ghost gets to a wall it turns in a random direction

--- game_objects/characters.py
+++ game_objects/characters.py
@@ -1,6 +1,8 @@
+from pygame.sprite import DirtySprite
 from . import GameObject
 from pygame.math import Vector2
 import pygame
+import random
 
 class Character(GameObject):
 
@@ -10,7 +12,7 @@ class Character(GameObject):
         self.velocity = Vector2()
         self.direction = Vector2()
 
-        self.movement_speed = 125
+        self.movement_speed = 85
         self.radius = 8
 
     
@@ -45,14 +47,14 @@ class Vaxman(Character):
         # Collisions with walls
         for wall in self.walls:
 
-            # Detecting collisions
+            # Detecting collisions with walls
 
             top = Vector2(self.position.x, self.position.y - self.radius)
             left = Vector2(self.position.x - self.radius, self.position.y)
             right = Vector2(self.position.x + self.radius, self.position.y)
             bottom = Vector2(self.position.x, self.position.y + self.radius)
 
-
+            # top point of vaxman collisioning with top wall
             if top.x >= wall.rect.left and top.x <= wall.rect.right and\
                 top.y <= wall.rect.bottom and top.y >= wall.rect.top:
 
@@ -105,7 +107,7 @@ class Vaxman(Character):
 
 class Ghost(Character):
 
-    MUTLIPLICATION_TIMER = 30.0
+    MUTLIPLICATION_TIME = 30.0
 
     def __init__(self, vaxman, game):
         super().__init__()
@@ -116,14 +118,29 @@ class Ghost(Character):
         self.game = game
 
         # countdown for ghost multiplication
-        self.counter = 0.0
+        self.multiplication_timer = 0.0
+        
+
+    # Changes the direction to a random direction different to current dirrection
+    def _change_dir_randomly(self):
+        posible_directions = [Vector2(0, 1), Vector2(0, -1), Vector2(1, 0), Vector2(-1, 0)]
+
+        directions_available = []
+
+        for direction in posible_directions:
+            if self.direction.x != direction.x or self.direction.y != direction.y:
+                directions_available.append(direction)
+
+        self.direction = directions_available[random.randint(0, len(directions_available) - 1)]
 
     
     def update(self, deltaTime):
-        self.counter += deltaTime
+        self.multiplication_timer += deltaTime
 
-        if self.counter >= Ghost.MUTLIPLICATION_TIMER:
-            self.counter = 0
+        print(self.multiplication_timer)
+
+        if self.multiplication_timer >= Ghost.MUTLIPLICATION_TIME:
+            self.multiplication_timer = 0
 
             new_ghost = Ghost(self.vaxman, self.game)
             new_ghost.position = Vector2(self.position)
@@ -147,26 +164,29 @@ class Ghost(Character):
             if top.x >= wall.rect.left and top.x <= wall.rect.right and\
                 top.y <= wall.rect.bottom and top.y >= wall.rect.top:
 
-                self.direction.xy = (0, 0)
                 self.position.y += 1
+                self._change_dir_randomly()
+
 
             if left.x >= wall.rect.left and left.x <= wall.rect.right and \
                 left.y >= wall.rect.top and left.y <= wall.rect.bottom:
 
-                self.direction.xy = (0, 0)
                 self.position.x += 1
+                self._change_dir_randomly()
+
 
             if right.x >= wall.rect.left and right.x <= wall.rect.right and \
                 right.y >= wall.rect.top and right.y <= wall.rect.bottom:
 
-                self.direction.xy = (0, 0)
                 self.position.x -= 1
+                self._change_dir_randomly()
+
 
             if bottom.x >= wall.rect.left and bottom.x <= wall.rect.right and \
                 bottom.y >= wall.rect.top and bottom.y <= wall.rect.bottom:
 
-                self.direction.xy = (0, 0)
                 self.position.y -= 1
+                self._change_dir_randomly()
 
 
         super().update(deltaTime)
