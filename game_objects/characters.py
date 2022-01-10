c9b376cfb844f36ecf0e89c0bbe3d09ba4334e67
Antonio Escorcia <packageinstaller2@gmail.com>
2022-01-10 14:22:27 -0600

    Ghost now multiply
    There is a timer for ghost to multiply currently set to 30 secs

--- game_objects/characters.py
+++ game_objects/characters.py
@@ -1,6 +1,4 @@
-from pygame.sprite import DirtySprite
 from . import GameObject
-from game import Game
 from pygame.math import Vector2
 import pygame
 
@@ -107,17 +105,73 @@ class Vaxman(Character):
 
 class Ghost(Character):
 
-    def __init__(self, vaxman):
+    MUTLIPLICATION_TIMER = 30.0
+
+    def __init__(self, vaxman, game):
         super().__init__()
 
         self.vaxman = vaxman
+        self.walls = []
+
+        self.game = game
+
+        # countdown for ghost multiplication
+        self.counter = 0.0
 
     
     def update(self, deltaTime):
+        self.counter += deltaTime
+
+        if self.counter >= Ghost.MUTLIPLICATION_TIMER:
+            self.counter = 0
+
+            new_ghost = Ghost(self.vaxman, self.game)
+            new_ghost.position = Vector2(self.position)
+            new_ghost.direction = Vector2(self.direction * -1)
+            new_ghost.walls = self.walls
+
+            self.game.add_game_object(new_ghost)
+
+
+        # Collisions with walls
+        for wall in self.walls:
+
+            # Detecting collisions
+
+            top = Vector2(self.position.x, self.position.y - self.radius)
+            left = Vector2(self.position.x - self.radius, self.position.y)
+            right = Vector2(self.position.x + self.radius, self.position.y)
+            bottom = Vector2(self.position.x, self.position.y + self.radius)
+
+
+            if top.x >= wall.rect.left and top.x <= wall.rect.right and\
+                top.y <= wall.rect.bottom and top.y >= wall.rect.top:
+
+                self.direction.xy = (0, 0)
+                self.position.y += 1
+
+            if left.x >= wall.rect.left and left.x <= wall.rect.right and \
+                left.y >= wall.rect.top and left.y <= wall.rect.bottom:
+
+                self.direction.xy = (0, 0)
+                self.position.x += 1
+
+            if right.x >= wall.rect.left and right.x <= wall.rect.right and \
+                right.y >= wall.rect.top and right.y <= wall.rect.bottom:
+
+                self.direction.xy = (0, 0)
+                self.position.x -= 1
+
+            if bottom.x >= wall.rect.left and bottom.x <= wall.rect.right and \
+                bottom.y >= wall.rect.top and bottom.y <= wall.rect.bottom:
+
+                self.direction.xy = (0, 0)
+                self.position.y -= 1
+
+
         super().update(deltaTime)
 
         distance = self.position.distance_to(self.vaxman.position)
 
         if distance <= (self.radius + self.vaxman.radius):
-            print("Covid destroyed")
             self.must_destroy = True
